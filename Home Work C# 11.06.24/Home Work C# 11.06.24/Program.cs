f8d9a43fedf77da0c1314d2b0c1c524ba48551a1
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-06-08 11:53:04 +0200

    Add files via upload

---
+++ Home Work C# 11.06.24/Home Work C# 11.06.24/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Home_Work_C__11._06._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mukhammed Lolo\n04.02.2005\n");
+            Console.WriteLine("City, Erlangen Germany Temporary Residence ∙ lmecomcompany@gmail.com ∙ 0687680685 ∙\n\n");
+            Console.WriteLine("WORK EXPERIENCE\nNo experience in firms\n\n");
+            Console.WriteLine("Education\nStudent of the Odessa branch of “IT STEP” (now)\r\nSpecialty: Software Development\n\n");
+            Console.WriteLine("SKILLS\nLanguages: C++, Java\r\nTools: Git, GitHub\r\nDatabase: PostgreSQL, JDBC\r\nOther: S.O.L.I.D, OOP, Design Patterns\n\n");
+            Console.WriteLine("About\r\nAn aspiring software developer with a great desire\r\nto learn and work in a team environment\r\nStudying with an excellent performance in “IT STEP”\r\n" +
+                "I have a good understanding of OOP and S.O.L.I.D\r\nI have experience of working in a team,\r\nwhich was gained during the final projects in “IT SPET”\r\n" +
+                "Languages: Ukrainian (fluent), Russian (fluent),\r\nEnglish (below average)\r\nLooking for a remote job (full time)\n\n");
+            Console.WriteLine("Links\nGitHub: https://github.com/thewhitemage13\r\nLinkedIn: https://www.linkedin.com/in/lolo-mukhammed-a63556294/");
+        }
+    }
+}
